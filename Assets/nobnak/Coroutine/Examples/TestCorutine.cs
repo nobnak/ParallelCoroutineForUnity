8887abe07ca84054a4860a9a8f8976dae13bef94
nobu.nakata <nobu.nakta@gmail.com>
2014-06-12 14:00:36 +0900

    no message

---
+++ Assets/nobnak/Coroutine/Examples/TestCorutine.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using nobnak.Coroutine;
+
+public class TestCorutine : MonoBehaviour {
+	private bool _started = false;
+
+	void OnGUI() {
+		GUILayout.BeginVertical(GUILayout.Width(200));
+
+		if (_started) {
+			if (GUILayout.Button("Stop"))
+				_started = false;
+		} else {
+			if (GUILayout.Button("Start")) {
+				_started = true;
+				StartCoroutine(Go ());
+			}
+		}
+
+		GUILayout.EndVertical();
+	}
+
+	IEnumerator Go () {
+		Debug.Log("Start");
+		var c = ParallelCoroutine.Build(this, Process("A", 0.3f), Process("B", 0.5f), Process("C", 0.7f));
+		yield return StartCoroutine(c);
+		Debug.Log("Done");
+	}
+
+	IEnumerator Process(string label, float wait) {
+		yield return null;
+		var counter = 0;
+		while (_started) {
+			Debug.Log(string.Format("{0} : {1:d}", label, ++counter));
+			yield return new WaitForSeconds(wait);
+		}
+	}
+
+}
---
+++ Assets/nobnak/Coroutine/ParallelCoroutine.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace nobnak.Coroutine {
+
+	public static class ParallelCoroutine {
+		public static IEnumerator Build(MonoBehaviour target, params IEnumerator[] coroutines) {
+			var monitors = new Monitor[coroutines.Length];
+			for (var i = 0; i < coroutines.Length; i++)
+				target.StartCoroutine(monitors[i] = new Monitor(coroutines[i]));
+			while (!AllDone(monitors))
+				yield return null;
+		}
+
+		public static bool AllDone(Monitor[] monitors) {
+			foreach (var m in monitors)
+				if (m.Progress != Monitor.State.Done)
+					return false;
+			return true;
+		}
+	}
+
+
+	public class Monitor : IEnumerator {
+		public enum State { Reset = 0, Playing, Done }
+
+		private IEnumerator _coroutine;
+
+		public Monitor(IEnumerator coroutine) {
+			this._coroutine = coroutine;
+		}
+
+		public State Progress { get; private set; }
+
+		#region IEnumerator implementation
+		public bool MoveNext () { 
+			var flag = _coroutine.MoveNext(); 
+			Progress = (flag ? State.Playing : State.Done);
+			return flag;
+		}
+		public void Reset () { 
+			Progress = State.Reset;
+			_coroutine.Reset(); 
+		}
+		public object Current { get { return _coroutine.Current; } }
+
+		#endregion
+	}
+}
